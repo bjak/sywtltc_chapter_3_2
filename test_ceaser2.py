9a6acd3f3b24d2f59a9266877c8fb8085beeef88
bjak <blake_jakobsson@utexas.edu>
2017-09-05 15:39:11 -0500

    Bad Test for Pylint Hook

--- test_ceaser2.py
+++ test_ceaser2.py
@@ -3,9 +3,9 @@
 import ceaser
 
 def test_encode2():
-    """Test for Encode"""
+    
     assert ceaser.encode("luke", 3) == "oxnh"
 
 def test_decode2():
-    """Test for Decode"""
+    
     assert ceaser.decode("oxnh", 3) == "luke"
