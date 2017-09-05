20897c1cbb17f7b6b852449b8fd040d63c9595d8
bjak <blake_jakobsson@utexas.edu>
2017-09-05 17:34:17 -0500

    Test Pylint Hook

--- test_ceaser2.py
+++ test_ceaser2.py
@@ -3,9 +3,9 @@
 import ceaser
 
 def test_encode2():
-    
+    """Test 1"""    
     assert ceaser.encode("luke", 3) == "oxnh"
 
 def test_decode2():
-    
+    """Test 2"""
     assert ceaser.decode("oxnh", 3) == "luke"
